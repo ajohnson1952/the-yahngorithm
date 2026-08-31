54ea72725008a6b3528da314112ba38da021f261
Drew <andrew.j.a1@gmail.com>
2026-08-31 13:01:58 -0500

    Avatar slot: /joe.jpg

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <header className="topbar">
           <div className="wrap topbar-inner">
             <a href="/" className="brand">
-              <img className="brand-avatar" src="/joe.png" alt="" />
+              <img className="brand-avatar" src="/joe.jpg" alt="" />
               the yahngorithm
             </a>
             <Nav />
